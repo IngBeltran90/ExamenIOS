5eb7968eabd3c8bb3d42a636ae3ea7a4614665d0
“IngBeltran90git config --global user.email “ing_jorge_beltran@live.com <YOUR@EMAIL.com>
2016-08-28 16:07:07 -0500

    Initial Commit

---
+++ ExamenIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExamenIOS
+//
+//  Created by Philbert on 8/28/16.
+//  Copyright © 2016 Philbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
